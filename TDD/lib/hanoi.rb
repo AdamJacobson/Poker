d897fcfd7fb35045c5a8fc300f09ae0ad3dcd0be
App Academy Student <student@appacademy.io>
2017-06-07 14:45:24 -0700

    Created working Hanoi game with specs

--- TDD/lib/hanoi.rb
+++ TDD/lib/hanoi.rb
@@ -1,5 +1,46 @@
 class Hanoi
+  attr_accessor :towers
+
   def initialize
-    @arrays = [[4, 3, 2, 1], [], []]
+    @towers = [[4, 3, 2, 1], [], []]
   end
+
+  def play
+    until won?
+      begin
+        p @towers
+        puts "Enter starting and ending column"
+        input = gets.chomp
+        first, last = input.split(",").map(&:to_i)
+
+        move(first, last)
+      rescue ArgumentError
+        puts "That input is not valid"
+        retry
+      end
+    end
+  end
+
+  def move(first, last)
+    raise ArgumentError if first > 2 || last > 2
+    if valid_move?(first, last)
+      @towers[last].push(@towers[first].pop)
+    else
+      puts "You can't move there"
+    end
+  end
+
+  def won?
+    @towers[0].empty? && @towers[1].length == 4 || @towers[2].length == 4
+  end
+
+  private
+  def valid_move?(first, last)
+    @towers[last].empty? || @towers[first].last < @towers[last].last
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  game = Hanoi.new
+  game.play
 end
--- TDD/spec/hanoi_spec.rb
+++ TDD/spec/hanoi_spec.rb
@@ -2,6 +2,55 @@ require "rspec"
 require "hanoi"
 
 describe Hanoi do
-  context "#" do
+  let(:hanoi) { Hanoi.new }
+  context "#move" do
+    context "with invalid input" do
+      it "raises an error" do
+        expect { hanoi.move(3, 1) }.to raise_error(ArgumentError)
+      end
 
+      it "Doesn't modify the towers" do
+        towers = hanoi.towers
+        expect { hanoi.move(3, 1) }.to raise_error(ArgumentError)
+        expect(hanoi.towers).to eq(towers)
+      end
+    end
+
+    context "With valid input" do
+      context "with a vaild move" do
+        it "moves top stack to new position" do
+          hanoi.move(0, 1)
+          towers = hanoi.towers
+          expect(towers[0].last).to eq(2)
+          expect(towers[1].last).to eq(1)
+        end
+      end
+
+      context "with an invalid move" do
+        it "prevents moving larger disc onto smaller disc" do
+          hanoi.move(0, 1) # valid
+          towers = hanoi.towers
+          hanoi.move(0, 1) # not valid
+
+          expect(hanoi.towers).to be(towers)
+        end
+      end
+    end
+  end
+
+  context "#won?" do
+    it "should be false when game starts" do
+      expect(hanoi.won?).to be(false)
+    end
+
+    it "should be true when all discs are on one tower other than starting tower." do
+      hanoi.towers = [[], [4,3,2,1], []]
+      expect(hanoi.won?).to be(true)
+    end
+
+    it "false when all discs are dispursed among towers other than starting tower" do
+      hanoi.towers = [[], [4,3], [2,1]]
+      expect(hanoi.won?).to be(false)
+    end
+  end
 end
